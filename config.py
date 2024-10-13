3c0f58025207467b45e8240a3904a96230411ccf
farah orfaly <farah.orfaly@gmail.com>
2024-10-13 23:42:40 +0200

    Updating test_create_account and db url

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
 
 class LocalConfig(Config):
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'.db', 'local.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'instance', 'local.db')
     DEBUG = True
 
 class GithubCIConfig(Config):
